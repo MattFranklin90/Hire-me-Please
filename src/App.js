d2867c9d77a0e5c81a558293e160c2f70cbb9354
mattfranklin <matt.franklin1990.mf@gmail.com>
2025-02-08 15:07:22 -0500

    I hope this worked

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hire Me, Please</h1>
+      <p>Welcome to your job tracking app!</p>
+    </div>
+  );
+}
+
+export default App;
