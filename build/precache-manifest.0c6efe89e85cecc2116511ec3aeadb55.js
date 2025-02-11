46563c09207eb3736b66078603f43cc9ec2f77b7
Margaux Glovier <margauxglovier@gmail.com>
2025-02-10 19:37:22 -0500

    node version files for render

---
+++ build/precache-manifest.0c6efe89e85cecc2116511ec3aeadb55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec0fae53b7bd293023b1c00497d811ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0509e695cf34bb3b56d",
+    "url": "/static/css/main.5d5401d7.chunk.css"
+  },
+  {
+    "revision": "b0509e695cf34bb3b56d",
+    "url": "/static/js/main.b24c8efc.chunk.js"
+  },
+  {
+    "revision": "57d37ef59754c8c06d21d7784de5032c",
+    "url": "/static/js/main.b24c8efc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b812b541910d29fa2648",
+    "url": "/static/js/runtime-main.d999991c.js"
+  }
+]);
